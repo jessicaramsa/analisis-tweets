c4f4bc1bb706c8c36dd73245d3bc7e09f148cbd1
Jéssica Ramírez <jessicaramsa@gmail.com>
2019-03-30 12:34:30 -0600

    Cambio en el guardado del sentimiento

--- common/models/resultado-analisis.js
+++ common/models/resultado-analisis.js
@@ -4,28 +4,29 @@ module.exports = function(Resultadoanalisis) {
   const superagent = require('superagent');
   Resultadoanalisis.peticionAnalisis = function(idPrincipal, documento, cb) {
       //console.log(cb);
-    
-    
-    Resultadoanalisis.create(documento, function(err, resultado) {
-        if(err) {
-            return cb(err);
-        } else {
-            delete documento.document.date;
-            console.log('documento: ', documento);
-            superagent
-            .post('https://language.googleapis.com/v1/documents:analyzeSentiment?key=AIzaSyBdXTsP99oTtcHGOVuFTRRsGQYMB262sa4')
-            .set('Content-Type', 'application/json')
-            .send(JSON.stringify(documento))
-            .end((err, res) => {
-              if (err) {  console.log('errrr ===============================', err);
+    let date = documento.document.date;
+    delete documento.document.date;
+    superagent
+      .post('https://language.googleapis.com/v1/documents:analyzeSentiment?key=AIzaSyBdXTsP99oTtcHGOVuFTRRsGQYMB262sa4')
+      .set('Content-Type', 'application/json')
+      .send(JSON.stringify(documento))
+       .end((err, res) => {
+          if (err) {  console.log('errrr ===============================', err);
                 return cb(err);
+              } else {
+                documento.document.date = date;
+                documento.document.idPrincipal = idPrincipal;
+                documento.document.sentiment = res.body.sentences[0].sentiment;
+                Resultadoanalisis.create(documento, function(err, resultado) {
+                    if(err) {
+                        return cb(err);
+                    } else {
+                        console.log('documento: ', documento);
+                        return cb(null, res.body.sentences);
+                    }
+                });   
               }
-              //console.log(res.body.sentences);
-              return cb(null, res.body.sentences);
-            })
-        }
-    });
-    
+        });
   };
 
   Resultadoanalisis.remoteMethod('peticionAnalisis', {
